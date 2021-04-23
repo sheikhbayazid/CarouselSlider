e7c8ab2b5e6d1285724d6fb3a808cbf7f68e0f40
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-04-24 00:36:27 +0600

    Initial Commit

---
+++ CarouselSlider/CarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSliderApp.swift
+//  CarouselSlider
+//
+//  Created by Sheikh Bayazid on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
